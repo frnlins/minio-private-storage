d7576830581c46b6c30f9fb3c5b1c477de4405ea
Filipe Lins <frnlins@gmail.com>
2020-04-06 12:10:21 -0300

    adding starter project

---
+++ minioPrivateStorage/src/main/java/com/filipelins/minioprivatestorage/MinioPrivateStorageApplication.java
@@ -0,0 +1,13 @@
+package com.filipelins.minioprivatestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioPrivateStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioPrivateStorageApplication.class, args);
+	}
+
+}
---
+++ minioPrivateStorage/src/test/java/com/filipelins/minioprivatestorage/MinioPrivateStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipelins.minioprivatestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioPrivateStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
